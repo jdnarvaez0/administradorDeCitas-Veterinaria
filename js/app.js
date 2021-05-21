4c07424493052e7827db53ae538b5f7310a3369e
navidev0 <juand.ns07@gmail.com>
2021-05-20 21:59:42 -0300

    agregando JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,93 @@
+// campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
 
+//IU
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+class Citas{
+    constructor(){
+        this.cistas = [];
+    }
+
+}
+
+class UI{
+    imprimirAlerta(mensaje, tipo) {
+        // Crea el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12', 'mt-3');
+        
+        // Si es de tipo error agrega una clase
+        if(tipo === 'error') {
+             divMensaje.classList.add('alert-danger');
+        } else {
+             divMensaje.classList.add('alert-success');
+        }
+         // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Insertar en el DOM
+        document.querySelector('#contenido').insertBefore( divMensaje , document.querySelector('.agregar-cita'));
+
+        // Quitar el alert despues de 3 segundos
+        setTimeout( () => {
+            divMensaje.remove();
+        }, 5000);
+    }
+}
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
+}
+
+//objeto con la info cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora:'',
+    sintomas: ''
+}
+
+
+// agrega datos al objeto cita
+function datosCita(e) {
+     citaObj[e.target.name] = e.target.value;
+     console.log(citaObj);
+}
+
+
+// valida y agrega una nueva cita a la clase citas
+function nuevaCita(e) {
+    e.preventDefault();
+
+    const {mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+
+    //validar
+    if( mascota === '' || propietario === '' || telefono === '' || fecha === ''  || hora === '' || sintomas === '' ) {
+        ui.imprimirAlerta('Todos los mensajes son Obligatorios', 'error')
+
+        return;
+    }
+    
+}
